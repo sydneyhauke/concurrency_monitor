c8544156e3cdf2b67c35b51adcb548e35dc81300
Sydney Hauke <sydney.hauke@gmail.com>
2017-05-31 15:03:15 +0200

    Implementation semaphore

--- src/osemaphore.cpp
+++ src/osemaphore.cpp
@@ -1,6 +1,27 @@
 #include "osemaphore.h"
 
-OSemaphore::OSemaphore(int n)
-{
+OSemaphore::OSemaphore(int n) : sem(n), v(n) {}
 
+void OSemaphore::acquire() {
+    v--;
+    sem.acquire();
+}
+
+void OSemaphore::release() {
+    v++;
+    sem.release();
+}
+
+bool OSemaphore::tryAcquire() {
+    bool hasAcquired = sem.tryAcquire();
+
+    if(hasAcquired) {
+        v--;
+    }
+
+    return hasAcquired;
+}
+
+int OSemaphore::semVal() {
+    return v;
 }
