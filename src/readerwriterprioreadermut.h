20bb2726f5de863e2d789cae5c75cc6060c84c77
Thuy-My <thuy-my.tran@epfl.ch>
2017-06-05 14:54:58 +0200

    Prio reader mutex & prio writer mutex

--- src/readerwriterprioreadermut.h
+++ src/readerwriterprioreadermut.h
@@ -1,11 +1,23 @@
 #ifndef READERWRITERPRIOREADERMUT_H
 #define READERWRITERPRIOREADERMUT_H
 
+#include "omutex.h"
+#include "ireaderwriter.h"
 
 class readerwriterprioreadermut
 {
+protected:
+    OMutex mutexReaders;
+    OMutex mutexWriters;
+    OMutex writer;
+    int nbReaders;
+
 public:
     readerwriterprioreadermut();
+    virtual void lockReader();
+    virtual void unlockReader();
+    virtual void lockWriter();
+    virtual void unlockWriter();
 };
 
 #endif // READERWRITERPRIOREADERMUT_H
--- src/readerwriterpriowritermut.h
+++ src/readerwriterpriowritermut.h
@@ -1,11 +1,25 @@
 #ifndef READERWRITERPRIOWRITERMUT_H
 #define READERWRITERPRIOWRITERMUT_H
 
+#include "omutex.h"
+#include "ireaderwriter.h"
 
 class readerwriterpriowritermut
 {
+protected:
+    OMutex mutexReaders;
+    OMutex mutexWriters;
+    OMutex writer;
+    OMutex reader;
+    OMutex mutex;
+    int nbReaders, nbWriters;
+
 public:
     readerwriterpriowritermut();
+    virtual void lockReader();
+    virtual void unlockReader();
+    virtual void lockWriter();
+    virtual void unlockWriter();
 };
 
 #endif // READERWRITERPRIOWRITERMUT_H
