1ebd65dd8db7e3de6331ca100bcc27f45962ae77
Thuy-My <thuy-my.tran@epfl.ch>
2017-06-07 15:46:18 +0200

    Equal prio mutex

--- src/readerwriterequalmut.cpp
+++ src/readerwriterequalmut.cpp
@@ -1,6 +1,38 @@
 #include "readerwriterequalmut.h"
 
-readerwriterequalmut::readerwriterequalmut()
-{
+readerwriterequalmut::readerwriterequalmut() :
+    mutex(),
+    fifo(),
+    writer(),
+    nbReaders(0)
+{}
 
+readerwriterequalmut::lockReader() {
+    fifo.lock();
+    mutex.lock();
+    nbReaders++;
+    if(nbReaders++) {
+        writer.lock();
+    }
+    mutex.unlock();
+    fifo.unlock();
+}
+
+readerwriterequalmut::unlockReader() {
+    mutex.lock();
+    nbReaders--;
+    if(nbReaders == 0) {
+        writer.unlock();
+    }
+    mutex.unlock();
+}
+
+readerwriterequalmut::lockWriter() {
+    fifo.lock();
+    writer.lock();
+}
+
+readerwriterequalmut::unlockWriter() {
+    writer.unlock();
+    fifo.unlock();
 }
--- src/readerwriterequalmut.h
+++ src/readerwriterequalmut.h
@@ -1,11 +1,23 @@
 #ifndef READERWRITEREQUALMUT_H
 #define READERWRITEREQUALMUT_H
 
+#include "ireaderwriter.h"
+#include "omutex.h"
 
 class readerwriterequalmut
 {
+protected:
+    OMutex mutex;
+    OMutex fifo;
+    OMutex writer;
+    int nbReaders;
+
 public:
     readerwriterequalmut();
+    virtual void lockReader();
+    virtual void unlockReader();
+    virtual void lockWriter();
+    virtual void unlockWriter();
 };
 
 #endif // READERWRITEREQUALMUT_H
