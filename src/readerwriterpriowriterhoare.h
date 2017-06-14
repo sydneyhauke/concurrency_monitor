b75ab361e85e105a90275853c5ab0ea909d3eb9b
Denise Gemesio <denise.gemesio@hotmail.com>
2017-06-14 15:41:57 +0200

    cpp to h + cin in make

--- src/readerwriterpriowriterhoare.h
+++ src/readerwriterpriowriterhoare.h
@@ -1,11 +1,36 @@
 #ifndef READERWRITERPRIOWRITERHOARE_H
 #define READERWRITERPRIOWRITERHOARE_H
 
+#include "ireaderwriter.h"
 
-class readerwriterpriowriterhoare
+class readerwriterpriowriterhoare : public IReaderWriter
 {
 public:
     readerwriterpriowriterhoare();
+
+    virtual void lockReader() {
+        monitorIn();
+
+        monitorOut();
+    }
+
+    virtual void unlockReader() {
+        monitorIn();
+
+        monitorOut();
+    }
+
+    virtual void lockWriter() {
+        monitorIn();
+
+        monitorOut();
+    }
+
+    virtual void unlockWriter() {
+        monitorIn();
+
+        monitorOut();
+    }
 };
 
 #endif // READERWRITERPRIOWRITERHOARE_H
