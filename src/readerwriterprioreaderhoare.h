01f3a49d27d95f374149bd163dfe6f98cd3b3a6a
Thuy-My <thuy-my.tran@epfl.ch>
2017-06-17 11:37:19 +0200

    Prio reader Hoare

--- src/readerwriterprioreaderhoare.h
+++ src/readerwriterprioreaderhoare.h
@@ -2,34 +2,44 @@
 #define READERWRITERPRIOREADERHOARE_H
 
 #include "ireaderwriter.h"
+#include "ohoaremonitor.h"
 
-
-class readerwriterprioreaderhoare : public IReaderWriter
+class readerwriterprioreaderhoare : public IReaderWriter, public OHoareMonitor
 {
+protected:
+    Condition writer;
+    int nbReaders;
+
 public:
     readerwriterprioreaderhoare();
 
     virtual void lockReader() {
         monitorIn();
-
+        nbReaders++;
+        if(nbReaders == 1) {
+            wait(writer);
+        }
         monitorOut();
     }
 
     virtual void unlockReader() {
         monitorIn();
-
+        nbReaders--;
+        if(nbReaders == 0) {
+            signal(writer);
+        }
         monitorOut();
     }
 
     virtual void lockWriter() {
         monitorIn();
-
+        wait(writer);
         monitorOut();
     }
 
     virtual void unlockWriter() {
         monitorIn();
-
+        signal(writer);
         monitorOut();
     }
 };
