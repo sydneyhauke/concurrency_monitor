7b68d9b91e238fd98ef1cf6ca5466eaf2cafee4d
Denise Gemesio <denise.gemesio@hotmail.com>
2017-06-17 10:15:27 +0200

    Equal Hoare done

--- src/readerwriterequalhoare.h
+++ src/readerwriterequalhoare.h
@@ -1,3 +1,15 @@
+/**
+  \file cppmain.cpp
+  \author Yosra Harbaoui et Denise Gemesio
+  \date 26.04.2017
+  \brief Classe main configurant les données de base des locomotives
+  et démarrant le programme
+  *
+  * Nous n'avons ici pas besoin d'utiliser un mutex vu que le monitorIn/Out se charge
+  * de ce rôle.
+  *
+*/
+
 #ifndef READERWRITEREQUALHOARE_H
 #define READERWRITEREQUALHOARE_H
 
@@ -5,42 +17,53 @@
 #include "osemaphore.h"
 #include "ohoaremonitor.h"
 
+
 class readerwriterequalhoare : public IReaderWriter
 {
 protected:
     OSemaphore fifo;
-    OSemaphore mutex;
     OHoareMonitor writer;
-    int readers;
+    OHoareMonitor::Condition accessing;
+    int nbReaders;
 
 public:
     readerwriterequalhoare() :
         fifo(1),
-        mutex(1),
-        readers(0)
+        nbReaders(0)
     {}
 
     virtual void lockReader() {
         monitorIn();
-
+        fifo.acquire();
+        nbReaders++;
+        if (nbReaders == 1) {
+            wait(accessing);
+        }
+        fifo.release();
         monitorOut();
     }
 
     virtual void unlockReader() {
         monitorIn();
-
+        nbReaders--;
+        if (nbReaders == 0) {
+            OHoareMonitor::signal(accessing);
+        }
+        fifo.release();
         monitorOut();
     }
 
     virtual void lockWriter() {
         monitorIn();
-
+        fifo.acquire();
+        wait(accessing);
         monitorOut();
     }
 
     virtual void unlockWriter() {
         monitorIn();
-
+        OHoareMonitor::signal(accessing);
+        fifo.release();
         monitorOut();
     }
 };
