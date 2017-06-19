49ca79fd5d54e1fcddd6eca4de2adb7a9cf3fb80
Denise Gemesio <denise.gemesio@hotmail.com>
2017-06-19 20:07:50 +0200

    Last changes

--- src/readerwriterprioreadingsem.h
+++ src/readerwriterprioreadingsem.h
@@ -8,7 +8,6 @@ class readerwriterprioreadingsem : public IReaderWriter
 {
 protected:
     OSemaphore mutex;
-    OSemaphore fifo;
     OSemaphore accessor;
     int nbReaders;
     int nbWriters;
@@ -17,7 +16,6 @@ protected:
 public:
     readerwriterprioreadingsem() :
         mutex(1),
-        fifo(1),
         accessor(1),
         nbReaders(0),
         nbWriters(0),
@@ -27,49 +25,27 @@ public:
     virtual void lockReader() {
         mutex.acquire();
         nbReaders++;
+        if (nbReaders == 1) {
+            accessor.acquire();
+        }
         mutex.release();
-        fifo.acquire();
-        accessor.acquire();
     }
 
     virtual void unlockReader() {
         mutex.acquire();
         nbReaders--;
-
-        if (nbReaders > 0 && !freeAllReaders) {
-
-            freeAllReaders = true;
-
-            for (int i = 0; i < nbReaders + nbWriters; ++i)
-                fifo.release();
-
-            for (int i = 0; i < nbReaders; ++i)
-                accessor.release();
-
-        } else if (nbReaders == 0) {
-            freeAllReaders = false;
+        if (nbReaders == 0) {
             accessor.release();
-            fifo.release();
         }
-
         mutex.release();
     }
 
     virtual void lockWriter() {
-        mutex.acquire();
-        nbWriters++;
-        mutex.release();
-        fifo.acquire();
-
-        if (freeAllReaders)
-            fifo.acquire();
-
         accessor.acquire();
     }
 
     virtual void unlockWriter() {
         accessor.release();
-        fifo.release();
     }};
 
 #endif // READERWRITERPRIOREADINGSEM_H
